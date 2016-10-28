64714e477e952eea42799dfe16fbaa0b380489ce
ETBCOR <ETBCOR@gmail.com>
2016-10-28 17:24:32 -0600

    making changes

---
+++ javascript/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+	var scrollSpeed = 500;
+	var scrollOffset = -150;
+
+	$("#header-button-coinspam").click(function () {
+		$('html, body').animate({
+			scrollTop: $("#coinspam").offset().top + scrollOffset
+		}, scrollSpeed);
+	});
+
+	$("#header-button-yt").click(function () {
+		$('html, body').animate({
+			scrollTop: $("#yt").offset().top + scrollOffset
+		}, scrollSpeed);
+	});
+
+	$("#header-button-welcome").click(function () {
+		$('html, body').animate({
+			scrollTop: $("#welcome-img").offset().top + scrollOffset
+		}, scrollSpeed);
+	});
+});
